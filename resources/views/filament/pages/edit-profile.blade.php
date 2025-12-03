6105b37c37a6a60e0432086637726ff21e554e24
Zoltán Tamás Szabó <zoli.szabok@gmail.com>
2025-12-03 14:13:40 +0100

    fix: Move submit button inside the form for better structure

--- resources/views/filament/pages/edit-profile.blade.php
+++ resources/views/filament/pages/edit-profile.blade.php
@@ -2,6 +2,12 @@
 
     <form wire:submit="save" class="max-w-3xl mx-auto space-y-6 bg-white dark:bg-gray-800 p-6 rounded-xl shadow-sm">
         {{ $this->form }}
+
+        <div class="flex justify-end mt-6">
+            <x-filament::button type="submit">
+                {{ __('Save') }}
+            </x-filament::button>
+        </div>
     </form>
 
     <x-filament-actions::modals />
